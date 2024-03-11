f795ddc7cb298e907fa5b1683609a8a2c2a8a109
Ángel Albiñana Espejo <angelo@MacBook-Air-Angelo.local>
2024-03-11 01:58:56 +0100

    :truck: chore(release): version packages

--- .github/workflows/action.sh
+++ .github/workflows/action.sh
@@ -6,7 +6,6 @@ version="v$1"
 # Check if branch v1 exists
 if ! git show-ref --verify --quiet "refs/heads/$major_version"; then
 	git checkout -b $major_version
-	git push origin $major_version
 fi
 
 # Add content to branch $major_version
@@ -18,23 +17,24 @@ create_release() {
     local v="$1"
     # Delete tag if it does exist
     if git rev-parse "$v" >/dev/null 2>&1; then
-        git tag :$v
+        git tag -d $v
     fi
     if git ls-remote --tags origin | grep -q "refs/tags/$v"; then
         git push origin :$v
     fi
+	#  create tag
 	git tag $v
 	git push origin --tags
 
     local release_name="BEPP action $v"
     local release_body="Release $v"
     
-    # Eliminar el lanzamiento si ya existe
+    # Delete release
     if gh release view "$v" >/dev/null 2>&1; then
         gh release delete "$v" --yes >/dev/null 2>&1 || true
     fi
 
-    # Crear el lanzamiento en GitHub
+    # Create release
     gh release create "$v" -t "$release_name" -n "$release_body"
 }
 
